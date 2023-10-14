d55322e286acb04ce9f04f74ec83c50e4b2166e6
Dimas Capelari <97951537+dimascapelari@users.noreply.github.com>
2023-10-14 16:28:07 -0300

    7_Req_HTTP_React

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,40 @@
 import "./App.css";
+import { useState, useEffect } from "react";
+
+const url = "http://localhost:3000/products";
+
+interface Product {
+  id: number;
+  name: string;
+  price: number;
+}
 
 function App() {
+  const [products, setProducts] = useState<Product[]>([]);
+
+  // 1- resgatando dados
+
+  useEffect(() => {
+    async function fetchData() {
+      const res = await fetch(url);
+      const data = await res.json();
+      setProducts(data);
+    }
+
+    fetchData();
+  }, []);
+
   return (
     <>
       <h1>Lista de Produtos</h1>
+
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>
+            {product.name} - R$: {product.price}
+          </li>
+        ))}
+      </ul>
     </>
   );
 }
